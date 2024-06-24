d575ad02cbe3ea728952bdd88ec8934802c3bc0c
Lea <leaventu@gmail.com>
2024-06-24 09:51:30 +0200

    ..

--- ajouter.php
+++ ajouter.php
@@ -5,7 +5,7 @@ require_once ("bdd.php");
 if (isset($_SESSION['ID_UTILISATEUR'])) {
     $id_perso = $_SESSION["ID_UTILISATEUR"];
     $ID = $_POST['id_livre'];
-
+    $date_ajout = $_POST['DATE_AJOUT'];
 
     $select_query = $connexion->prepare("SELECT * FROM livres WHERE ID = ?");
     $select_query->bind_param("s", $ID);
@@ -16,16 +16,16 @@ if (isset($_SESSION['ID_UTILISATEUR'])) {
         $row = $result->fetch_assoc();
         $select_query->close();
 
-        $update_query = $connexion->prepare("UPDATE livres SET ID_UTILISATEUR=?, NOM_LIVRE=?, TOME=?, AUTEUR=?, GENRE=?, EDITIONS=?, NOTE=?, DATE_AJOUT=?, IMAGES=? WHERE ID=?");
-        $update_query->bind_param("ssssssssss", $id_perso, $row['NOM_LIVRE'], $row['TOME'], $row['AUTEUR'], $row['GENRE'], $row['EDITIONS'], $row['NOTE'], $date_ajout, $row['IMAGES'], $ID);
+        $insert_query = $connexion->prepare("INSERT INTO livres (ID_UTILISATEUR, NOM_LIVRE, TOME, AUTEUR, GENRE, EDITIONS, NOTE, DATE_AJOUT, IMAGES) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $insert_query->bind_param("sssssssss", $id_perso, $row['NOM_LIVRE'], $row['TOME'], $row['AUTEUR'], $row['GENRE'], $row['EDITIONS'], $row['NOTE'], $date_ajout, $row['IMAGES']);
 
-        if ($update_query->execute()) {
-            echo json_encode(['success' => 'Livre mis à jour avec succès']);
+        if ($insert_query->execute()) {
+            echo json_encode(['success' => 'Livre ajouté avec succès']);
         } else {
-            echo json_encode(['error' => 'Erreur lors de la mise à jour : ' . $update_query->error]);
+            echo json_encode(['error' => 'Erreur lors de l\'ajout : ' . $insert_query->error]);
         }
 
-        $update_query->close();
+        $insert_query->close();
     } else {
         echo json_encode(['error' => 'Aucun résultat trouvé pour l\'ID : ' . $ID]);
     }
